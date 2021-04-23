5c227a4f9e8814586d5683bbc623aff08b45c254
261718 <80752698+261718@users.noreply.github.com>
2021-04-23 11:51:03 +0530

    first commit

---
+++ python programming submission/min max in a set.py
@@ -0,0 +1,5 @@
+set = set([5, 10, 3, 15, 2, 20])
+#Find maximum value
+print(max(set))
+#Find minimum value
+print(min(set))
---
+++ python programming submission/repeated items in a tuple.py
@@ -0,0 +1,6 @@
+#create a tuple
+tuple = 2, 4, 5, 6, 2, 3, 4, 4, 7 
+print(tuple)
+#return the number of times it appears in the tuple.
+count = tuple.count(4)
+print(count)
