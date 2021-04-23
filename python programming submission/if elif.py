b45c64a3d22429f120867f5c3908aa06dc8f757f
261718 <80752698+261718@users.noreply.github.com>
2021-04-23 11:31:12 +0530

    first commit

---
+++ python programming submission/if elif.py
@@ -0,0 +1,9 @@
+txt=input()
+if (txt=="lol"):
+    print("laughing out loud")
+elif (txt=="rofl"):
+    print("rolling on the floor laughing")
+elif (txt=="lmk"):
+    print("let me know")
+else:
+    print("shaking my head")
